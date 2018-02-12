fdf94b99dceec9af69b6f4ad21188b4a2e6d5c8b
shikhar-scs <shikhar9a@gmail.com>
2018-02-13 02:42:50 +0530

    working pdftotext

--- frontendWorks/selfJS/pdftotext.js
+++ frontendWorks/selfJS/pdftotext.js
@@ -2,7 +2,8 @@ var PDFExtract = require('pdf.js-extract').PDFExtract;
 var fs = require('fs')
 var pdfExtract = new PDFExtract();
 var options = {}; /* options are handed over to pdf.js e.g, { password: 'somepassword' } */
-pdfExtract.extract('./Re.pdf', options , function (err, data) {
+pdfExtract.extract('', options , function (err, data) {
+	
 	if (err) return console.log(err);
 	var hello = '';
 	for ( let i = 0 ; i < data.pages[0].content.length; i++)
@@ -10,5 +11,5 @@ pdfExtract.extract('./Re.pdf', options , function (err, data) {
 	
 	fs.writeFileSync('./example-output.json', JSON.stringify(hello, null, '\t'));
 	console.log(data.meta.pages);
-});
 
+});
