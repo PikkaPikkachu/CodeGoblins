70c2e0bccc79eb0f26009660001752d3eed7eb6c
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-02-13 08:08:51 +0530

    upload feature added

--- server.js
+++ server.js
@@ -2,8 +2,38 @@ const express = require('express');
 const config = require('./config.json');
 const bodyParser = require('body-parser');
 const path = require('path');
-const app = express();
+var app = express();
+var upload = require('express-fileupload');
 
+app.use(upload()); // configure middleware
+
+
+app.get('/uploadRESUME2',function(req,res){
+  res.sendFile(__dirname+'/frontendWorks/HTMLfiles/index3.html');
+})
+app.post('/upload',function(req,res){
+  console.log(req.files);
+  if(req.files.upfile){
+    var file = req.files.upfile,
+      name = file.name,
+      type = file.mimetype;
+    var uploadpath = __dirname + '/uploads/' + name;
+    file.mv(uploadpath,function(err){
+      if(err){
+        console.log("File Upload Failed",name,err);
+        res.send("Error Occured!")
+      }
+      else {
+        console.log("File Uploaded",name);
+        res.send('Done! Uploading files')
+      }
+    });
+  }
+  else {
+    res.send("No File selected !");
+    res.end();
+  };
+})
 const routes = {
 	login: require('./api/login').route,
 	form: require('./api/form').route,
